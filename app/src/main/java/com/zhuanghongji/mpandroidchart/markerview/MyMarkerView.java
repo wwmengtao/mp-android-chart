30f4b46cb6ceacd027702b99462e68e655494974
zhuanghongji <377232403@qq.com>
2015-12-30 13:19:19 +0800

    null

---
+++ app/src/main/java/com/zhuanghongji/mpandroidchart/markerview/MyMarkerView.java
@@ -0,0 +1,33 @@
+package com.zhuanghongji.mpandroidchart.markerview;
+
+import android.content.Context;
+import android.widget.TextView;
+
+import com.github.mikephil.charting.components.MarkerView;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.highlight.Highlight;
+import com.zhuanghongji.mpandroidchart.R;
+
+public class MyMarkerView extends MarkerView {
+    private TextView mContentTv;
+
+    public MyMarkerView(Context context, int layoutResource) {
+        super(context, layoutResource);
+        mContentTv = (TextView) findViewById(R.id.tv_content_marker_view);
+    }
+
+    @Override
+    public void refreshContent(Entry e, Highlight highlight) {
+        mContentTv.setText("" + e.getVal());
+    }
+
+    @Override
+    public int getXOffset(float xpos) {
+        return -(getWidth() / 2);
+    }
+
+    @Override
+    public int getYOffset(float ypos) {
+        return -getHeight();
+    }
+}
